2789561690964936809ae0126891ada36244675f
Carsten Thue-Bludworth <carstentb@protonmail.com>
2022-11-11 23:31:46 -0500

    Verified working on two computers; still working on basics

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::panic;
+use std::time::{Duration, Instant};
 use console_error_panic_hook;
 
 use winit::{
@@ -40,7 +41,6 @@ pub fn main() {
         panic::set_hook(Box::new(console_error_panic_hook::hook));
     }
 
-    ui::draw_text();
     println!("2");
     let el = EventLoop::new();
     // use winit_input_helper::WinitInputHelper;
@@ -53,32 +53,18 @@ pub fn main() {
 
     log::info!("Window size is {} x {}", window.inner_size().width, window.inner_size().height);
 
+    let graphics = ui::draw_text();
     /**************************************************************************
     This is the setup if compiling for x86_64 machines
     **************************************************************************/
     #[cfg(target_arch = "x86_64")]
     {
         use pixels_draw_target::Pixelbuffer;
-        let mut pb = Pixelbuffer::new(window);
-        
-        let circle = {
-            let style = PrimitiveStyleBuilder::new()
-                .stroke_color(Rgb888::RED)
-                .stroke_width(1)
-                .fill_color(Rgb888::GREEN)
-                .build();
-            Circle::new(Point::new(10, 10), 50)
-                .into_styled(style)
-        };
-
-        let text = {
-            let style = MonoTextStyle::new(&FONT_10X20, Rgb888::WHITE);
-            Text::new("hello Web Assembly!", Point::new(0, 15), style)
-        };
-
-        circle.draw(&mut pb).unwrap();
-        text.draw(&mut pb).unwrap();
-
+        let mut pixel_buffer = Pixelbuffer::new(window);
+        let start = Instant::now();
+        graphics.draw(&mut pixel_buffer).unwrap();
+        let duration = start.elapsed();
+        println!("Time elapsed in drawing graphics is: {:?}", duration);
     }
 
     /**************************************************************************
@@ -89,36 +75,18 @@ pub fn main() {
         use winit::platform::web::WindowExtWebSys;
         use canvas_display::CanvasDisplay;
 
-        let canvas = window.canvas();
-        let size = window.inner_size();
-        let window = web_sys::window().unwrap();
+        let canvas   = window.canvas();
+        let size     = window.inner_size();
+        let window   = web_sys::window().unwrap();
         let document = window.document().unwrap();
-        let body = document.body().unwrap();
+        let body     = document.body().unwrap();
 
         body.append_child(&canvas)
             .expect("Error appending canvas to HTML body :(");
 
-        let mut cd = CanvasDisplay::new(&canvas);
-        log::info!("CanvasDisplay size is {} x {}", cd.width, cd.height);
-
-        let circle = {
-            let style = PrimitiveStyleBuilder::new()
-                .stroke_color(Rgb888::RED)
-                .stroke_width(1)
-                .fill_color(Rgb888::GREEN)
-                .build();
-            Circle::new(Point::new(10, 10), 50)
-                .into_styled(style)
-        };
-
-        let text = {
-            let style = MonoTextStyle::new(&FONT_10X20, Rgb888::BLACK);
-            Text::new("hello Web Assembly!", Point::new(0, 15), style)
-        };
-
-        circle.draw(&mut cd).unwrap();
-        text.draw(&mut cd).unwrap();
-
+        let mut pixel_buffer = CanvasDisplay::new(&canvas);
+        log::info!("CanvasDisplay size is {} x {}", pixel_buffer.width, pixel_buffer.height);
+        graphics.draw(&mut pixel_buffer).unwrap();
     }
 
     /**************************************************************************
--- src/ui.rs
+++ src/ui.rs
@@ -2,10 +2,27 @@ use embedded_graphics::{
     prelude::*,
     text::Text,
     mono_font::{ascii::FONT_6X9, MonoTextStyle},
-    pixelcolor::BinaryColor,
+    pixelcolor::Rgb888,
 };
 
-pub fn draw_text() {
-    let style = MonoTextStyle::new(&FONT_6X9, BinaryColor::On);
-    let text = Text::new("hello embedded graphics through Wasm!", Point::new(0, 0), style);
+pub fn draw_text() -> Text<'static, MonoTextStyle<'static, Rgb888>> {
+    // Now construct the graphics
+    // let circle = {
+    //     let style = PrimitiveStyleBuilder::new()
+    //         .stroke_color(Rgb888::RED)
+    //         .stroke_width(1)
+    //         .fill_color(Rgb888::GREEN)
+    //         .build();
+    //     Circle::new(Point::new(10, 10), 50)
+    //         .into_styled(style)
+    // };
+
+    // let text = {
+    //     let style = MonoTextStyle::new(&FONT_10X20, Rgb888::WHITE);
+    //     Text::new("hello Web Ass!", Point::new(0, 15), style)
+    // };
+
+    let style = MonoTextStyle::new(&FONT_6X9, Rgb888::RED);
+    let text = Text::new("hello embedded graphics through Wasm!", Point::new(30, 30), style);
+    return text;
 }
